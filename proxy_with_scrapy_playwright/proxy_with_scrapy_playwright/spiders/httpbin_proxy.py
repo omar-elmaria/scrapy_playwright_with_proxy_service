4ea5243a6aeee07f6677feaa948b3edadb1acc04
omar-elmaria <omar.elmaria@mim2018.esmt.org>
2022-12-19 15:37:47 +0100

    add an errback and some settings

--- proxy_with_scrapy_playwright/proxy_with_scrapy_playwright/spiders/httpbin_proxy.py
+++ proxy_with_scrapy_playwright/proxy_with_scrapy_playwright/spiders/httpbin_proxy.py
@@ -1,7 +1,7 @@
 from scrapy import Spider, Request
+from scrapy.selector import Selector
 import os
 import re
-from w3lib.html import remove_tags
 from dotenv import load_dotenv
 
 # Load environment variables
@@ -11,6 +11,8 @@ load_dotenv()
 class ProxySpider(Spider):
     name = "httpbin"
     custom_settings = {
+        "FEED_EXPORT_ENCODING": "utf-8", # UTF-8 deals with all types of characters
+        "CONCURRENT_REQUESTS": 20,
         "PLAYWRIGHT_LAUNCH_OPTIONS": {
             "proxy": {
                 "server": f"http://scraperapi.country_code=de:{os.getenv(key='SCRAPER_API_KEY')}@proxy-server.scraperapi.com:8001",
@@ -27,15 +29,22 @@ class ProxySpider(Spider):
     }
 
     def start_requests(self):
-        for i in range(0, 200):
+        for i in range(0, 4000):
             yield Request(
                 "http://httpbin.org/user-agent",
                 meta={"playwright": True, "playwright_include_page": True},
                 callback=self.parse,
+                errback=self.errback_close_page,
                 dont_filter=True
             )
 
     async def parse(self, response):
         page = response.meta["playwright_page"] # The page object that allows us to interact with the webpage
-        yield {"user-agent": re.findall(pattern="(?<=\"user-agent\": \")(.*)(?=\")", string=response.xpath("//pre/text()").get())[0]}
+        s  = Selector(text=await page.content())
+        await page.close()
+        yield {"user-agent": re.findall(pattern="(?<=\"user-agent\": \")(.*)(?=\")", string=s.xpath("//pre/text()").get())[0]}        
+    
+    async def errback_close_page(self, failure):
+        page = failure.request.meta["playwright_page"]
+        self.logger.warning("There was an error when processing %s: %s", failure.request, failure.value)
         await page.close()
